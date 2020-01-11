c573b4e5e7ca4d61bae2dc8031bb1f0e28e56f86
rafael <rafaelpinheiropereira@gmail.com>
2020-01-11 10:36:03 -0300

    :construction: Primeira Versão de testes :bug: Adicionar strategy = GenerationType.AUTO no venseq

--- src/main/java/br/com/minasfrango/entity/Pedido.java
+++ src/main/java/br/com/minasfrango/entity/Pedido.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -18,6 +20,7 @@ import lombok.Data;
 public class Pedido implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "venseq", nullable = false)
     private long id;
 
